d7a724c5d332cb9b6c7c749ed0a77d2970ecbd29
Radiet <radiet@41studio.com>
2015-09-07 17:55:46 +0700

    Test push with ssh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  # Test push with ssh
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
