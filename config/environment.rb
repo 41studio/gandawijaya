1a53f566a71dfb6f7e9b52dfe3c9eec163cbe80a
Radiet <radiet@41studio.com>
2015-09-08 16:21:22 +0700

    fixing error setting mailer2

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
+Rails.application.configure do
+  config.action_mailer.default_url_options = { :host => 'gandawijaya.herokuapp.com' }
+end
