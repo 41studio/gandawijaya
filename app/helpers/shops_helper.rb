5c57a3837d90bc8aaac7589f5d800d88df4ba502
ian <alfian@41studio.com>
2015-12-08 10:36:40 +0700

    add like chart for products

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -3,9 +3,9 @@ module ShopsHelper
     Impression.where(impressionable_id: id, impressionable_type: type).group_by_day(:created_at, format: "%d %b %Y").count
   end
 
-  def product_impression_graph(id, type)
-    Impression.where(impressionable_id: id, impressionable_type: type).group_by_day(:created_at, format: "%d %b %Y").count
-  end
+  # def product_impression_graph(id, type)
+  #   Impression.where(impressionable_id: id, impressionable_type: type).group_by_day(:created_at, format: "%d %b %Y").count
+  # end
 
   def form_offer
     if user_signed_in?
